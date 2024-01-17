a4c84bda9909212a72d50c4c52496c9cc93a4cc7
Cyril Kamgais <102966559+Kamgais@users.noreply.github.com>
2024-01-17 01:30:08 +0100

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,7 @@
+import classes from './about.module.css';
+
+export default function About() {
+  return (
+    <h1 className={classes.title}>About</h1>
+  )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section>
+      <div className='container'>
+        <h1 className='text-3xl font-bold mt-5'>Nextjs 13 Starter Kit</h1>
+      </div>
+    </section>
+  )
+}
